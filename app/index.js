62ac1844dd3c53bdbe1eeeea2dc29a194662f018
YusofBandar <ybandar1902@gmail.com>
2020-01-18 13:26:54 +0000

    Drawing circle at centre

--- app/index.js
+++ app/index.js
@@ -4,7 +4,17 @@ window.onload = () => {
   getLocation(
     result => {
       const coords = result.coords;
-      map = initMap({ lat: coords.latitude, lng: coords.longitude });
+      const centre = { lat: coords.latitude, lng: coords.longitude };
+      map = initMap(centre);
+      let positionCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: centre,
+        radius: 40
+      });
     },
     () => {
       console.log("error getting location");
