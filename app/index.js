5d483744df4e4d6379fddaab02a1f1d8fa4f526c
YusofBandar <ybandar1902@gmail.com>
2020-01-18 13:55:51 +0000

    Adds map config options

--- app/index.js
+++ app/index.js
@@ -6,16 +6,7 @@ window.onload = () => {
       const coords = result.coords;
       const centre = { lat: coords.latitude, lng: coords.longitude };
       map = initMap(centre);
-      let positionCircle = new google.maps.Circle({
-        strokeColor: '#FF0000',
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        map: map,
-        center: centre,
-        radius: 40
-      });
-
+     
       map.addListener("projection_changed",() => {
           console.log("result", coordsToPixel(map, map.center));
       })
@@ -37,6 +28,9 @@ const getLocation = (succ, err) => {
 const initMap = (center, zoom = 15) => {
   return new google.maps.Map(document.getElementById("map"), {
     center,
-    zoom
+    zoom,
+    disableDefaultUI : true,
+    disableDoubleClickZoom : true,
+    draggable : false
   });
 };
