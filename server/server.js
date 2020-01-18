9c44dcc695bea22a519d0ee590e81916a5a7a599
YusofBandar <ybandar1902@gmail.com>
2020-01-18 21:10:29 +0000

    Inits towers get endpoint

--- server/server.js
+++ server/server.js
@@ -6,4 +6,20 @@ app.get("/", (req, res) => {
   res.send("Cell Towers API");
 });
 
+app.get("/towers", (req, res) => {
+  const query = req.query;
+  if (!("lat" in query)) {
+    res.statusMessage = "Missing lat query param"
+    res.status(400).end();
+    return;
+  }
+  if(!("long" in query)){
+    res.statusMessage = "Missing long query param"
+    res.status(400).end();
+    return;
+  }
+  
+  res.send("getting towers");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
