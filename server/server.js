26ab6eeae364bd97a676188bb11ec7be14394629
YusofBandar <ybandar1902@gmail.com>
2020-01-18 20:53:09 +0000

    Formats code
    no logic changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5501;
 
 app.get("/", (req, res) => {
-    res.send("Cell Towers API");
-})
+  res.send("Cell Towers API");
+});
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
