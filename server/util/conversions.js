c1203ef2f257097ba764917e680b732e6c80420b
YusofBandar <ybandar1902@gmail.com>
2020-01-19 11:35:27 +0000

    Adds useful coordinate functions

---
+++ server/util/conversions.js
@@ -0,0 +1,34 @@
+// taken from https://gis.stackexchange.com/a/2980
+const offsetCoordsMetres = (lat, lng, offset) => {
+  //Earth’s radius, sphere
+  const R = 6378137;
+
+  //Coordinate offsets in radians
+  const dLat = offset / R;
+  const dLng = offset / (R * Math.cos((Math.PI * lat) / 180));
+
+  //OffsetPosition, decimal degrees
+  const latO = lat + (dLat * 180) / Math.PI;
+  const lngO = lng + (dLng * 180) / Math.PI;
+
+  return [latO, lngO];
+};
+
+const coordsDistanceMetres = (lat1, lng1, lat2, lng2) => {
+    const R = 6378.137; // Radius of earth in KM
+    
+    const dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+    const dlng = lng2 * Math.PI / 180 - lng1 * Math.PI / 180;
+    
+    const a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dlng/2) * Math.sin(dlng/2);
+    
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    const d = R * c;
+    
+    return d * 1000; // meters
+}
+
+exports.offsetCoordsMetres = offsetCoordsMetres;
+exports.coordsDistanceMetres = coordsDistanceMetres;
