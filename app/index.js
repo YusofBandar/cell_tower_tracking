c4c3e7713dc00832a96f76f23ebaf00f9e104dc8
YusofBandar <ybandar1902@gmail.com>
2020-01-21 19:45:50 +0000

    Cell markers change opacity depending on chosen provider

--- app/index.js
+++ app/index.js
@@ -2,7 +2,11 @@ import api from "./api.js";
 import conversion from "./conversions.js";
 import mapStyling from "./mapStyling.js";
 
-const providers = ["EE", "Vodaphone", "O2"];
+const providers = [
+  { provider: "BT", net: [0, 76, 77] },
+  { provider: "O2", net: [2, 10, 11] },
+  { provider: "Vodaphone", net: [7, 15, 91, 92] }
+];
 
 window.onload = () => {
   let map;
@@ -10,9 +14,10 @@ window.onload = () => {
   let currentCoords;
   let originalCoords;
 
-  updateProviders(providers, "EE");
-
   let cellTowers = [];
+
+  let selectedProvider = providers[0];
+
   getLocation(
     result => {
       const coords = result.coords;
@@ -27,9 +32,14 @@ window.onload = () => {
 
       let overlay = new google.maps.OverlayView();
       overlay.draw = () => {
-        draw(overlay, currentCoords, cellTowers);
+        draw(overlay, currentCoords, cellTowers, selectedProvider);
       };
       overlay.setMap(map);
+
+      updateProviders(providers, "BT", provider => {
+        selectedProvider = provider;
+        draw(overlay, currentCoords, cellTowers, selectedProvider);
+      });
     },
     () => {
       console.log("error getting location");
@@ -56,17 +66,22 @@ window.onload = () => {
   });
 };
 
-const draw = (overlay, currentCoords, cellTowers) => {
+const draw = (overlay, currentCoords, cellTowers, provider) => {
   const pixelCentre = coordsToPixel(
     overlay,
     new google.maps.LatLng(currentCoords.lat, currentCoords.lng)
   );
 
-  updateCellTowerMarkers(
-    cellTowers.map(tower =>
-      coordsToPixel(overlay, new google.maps.LatLng(tower.lat, tower.lon))
-    )
-  );
+  let cellTowerMarkers = cellTowers.map(tower => {
+    let pixelCoord = coordsToPixel(
+      overlay,
+      new google.maps.LatLng(tower.lat, tower.lon)
+    );
+    tower.x = pixelCoord.x;
+    tower.y = pixelCoord.y;
+    return tower;
+  });
+  updateCellTowerMarkers(cellTowerMarkers, provider);
 
   updateLocationMarker(pixelCentre);
 };
@@ -108,21 +123,24 @@ const updateLocationMarker = pixelCoords => {
     .attr("cy", pixelCoords.y);
 };
 
-const updateProviders = (providers, selected) => {
+const updateProviders = (providers, selected, cb) => {
   d3.select(".providers")
     .selectAll("button")
     .data(providers)
     .enter()
     .append("button")
-    .html(d => d)
-    .on("click", d => updateProviders(providers, d));
+    .html(d => d.provider)
+    .on("click", d => {
+      cb(d);
+      updateProviders(providers, d.provider);
+    });
 
   d3.selectAll(".providers button").style("opacity", d =>
-    d === selected ? 1 : 0.2
+    d.provider === selected ? 1 : 0.2
   );
 };
 
-const updateCellTowerMarkers = cellTowers => {
+const updateCellTowerMarkers = (cellTowers, selectedProvider) => {
   d3.select("svg")
     .selectAll(".cellTower")
     .data(cellTowers)
@@ -136,7 +154,9 @@ const updateCellTowerMarkers = cellTowers => {
     .style("opacity", 0)
     .style("fill", "rgba(153, 241, 55)")
     .transition()
-    .style("opacity", 0.65)
+    .style("opacity", d => {
+      return selectedProvider.net.indexOf(Number(d.net)) < 0 ? 0.1 : 0.7;
+    })
     .attr("r", 8)
     .duration(1000)
     .delay(() => {
@@ -146,7 +166,10 @@ const updateCellTowerMarkers = cellTowers => {
 
   d3.selectAll(".cellTower circle")
     .attr("cx", (d, i) => cellTowers[i].x)
-    .attr("cy", (d, i) => cellTowers[i].y);
+    .attr("cy", (d, i) => cellTowers[i].y)
+    .style("opacity", d => {
+      return selectedProvider.net.indexOf(Number(d.net)) < 0 ? 0.1 : 0.7;
+    });
 };
 
 const initMap = (center, zoom = 15) => {
