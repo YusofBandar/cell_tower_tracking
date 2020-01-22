faa0ac60a8a39d3c77955f05bfad48ae55f3f7c4
YusofBandar <ybandar1902@gmail.com>
2020-01-22 11:23:02 +0000

    Formats conversions

--- app/conversions.js
+++ app/conversions.js
@@ -16,22 +16,25 @@ const offsetCoordsMetres = (lat, lng, northOffset, eastOffset) => {
 
 // taken from https://stackoverflow.com/a/11172685
 const coordsDistanceMetres = (lat1, lng1, lat2, lng2) => {
-    const R = 6378.137; // Radius of earth in KM
-    
-    const dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
-    const dlng = lng2 * Math.PI / 180 - lng1 * Math.PI / 180;
-    
-    const a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-    Math.sin(dlng/2) * Math.sin(dlng/2);
-    
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    const d = R * c;
-    
-    return d * 1000; // meters
-}
+  const R = 6378.137; // Radius of earth in KM
+
+  const dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
+  const dlng = (lng2 * Math.PI) / 180 - (lng1 * Math.PI) / 180;
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dlng / 2) *
+      Math.sin(dlng / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
+
+  return d * 1000; // meters
+};
 
 export default {
   offsetCoordsMetres,
   coordsDistanceMetres
-}
+};
