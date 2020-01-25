e454d80af325ef7062cc772dd4e4d51d97e0ccd9
YusofBandar <ybandar1902@gmail.com>
2020-01-25 11:57:00 +0000

    Formats code

--- app/api.js
+++ app/api.js
@@ -1,27 +1,28 @@
 const baseUrl = "http://127.0.0.1:5501";
 
 const getTowers = (lat, lng) => {
-    return getRequest(`${baseUrl}/towers?lat=${lat}&lng=${lng}`);
+  return getRequest(`${baseUrl}/towers?lat=${lat}&lng=${lng}`);
 };
 
 export default {
   getTowers
 };
 
-const getRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url, {
-            method: "get"
-          }).then(result => {
-              return result;
-          })
-          .then(result => {
-            result.json().then((r) => {
-                resolve(r);
-            })
-          })
-          .catch((err) => {
-              reject(err);
-          })
+const getRequest = url => {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "get"
     })
-}
+      .then(result => {
+        return result;
+      })
+      .then(result => {
+        result.json().then(r => {
+          resolve(r);
+        });
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
