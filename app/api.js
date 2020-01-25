77256909a51575449ec2cd4f2c78be23b61e6da8
YusofBandar <ybandar1902@gmail.com>
2020-01-25 12:16:32 +0000

    Adds geolocation post request

--- app/api.js
+++ app/api.js
@@ -4,6 +4,21 @@ const getTowers = (lat, lng) => {
   return getRequest(`${baseUrl}/towers?lat=${lat}&lng=${lng}`);
 };
 
+const getGeoLocation = (apiKey, carrier, cellTowers) => {
+  const body = {
+    homeMobileCountryCode: carrier.mcc,
+    homeMobileNetworkCode: carrier.mnc,
+    considerIp: "false",
+    carrier,
+    cellTowers
+  };
+
+  return postRequest(
+    `https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`,
+    body
+  );
+};
+
 const getRequest = url => {
   return new Promise((resolve, reject) => {
     fetch(url, {
@@ -23,6 +38,27 @@ const getRequest = url => {
   });
 };
 
+const postRequest = (url, body) => {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "get",
+      body: JSON.stringify(body)
+    })
+      .then(result => {
+        return result;
+      })
+      .then(result => {
+        result.json().then(r => {
+          resolve(r);
+        });
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
 export default {
-    getTowers
-  };
+  getTowers,
+  getGeoLocation
+};
