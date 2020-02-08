e6e4de1702cd010aeeb2f6f1edac64135a2c78a2
YusofBandar <ybandar1902@gmail.com>
2020-02-08 14:33:35 +0000

    Ngrok https server

--- app/api.js
+++ app/api.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://1994dc88.ngrok.io";
+const baseUrl = "https://1994dc88.ngrok.io";
 
 const getTowers = (lat, lng) => {
   return getRequest(`${baseUrl}/towers?lat=${lat}&lng=${lng}`);
