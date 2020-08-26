16e99fc7e94f9686f1266125bf79b05c24b85ec0
Yusof Bandar <yusof.bandar@squaredup.com>
2020-08-26 22:57:49 +0100

    Adds location to pixel coords to useMap hook

--- cell-tower-tracking/src/hooks/useMap.js
+++ cell-tower-tracking/src/hooks/useMap.js
@@ -1,25 +1,52 @@
 import {useRef, useState, useEffect} from 'react';
 import MapStyles from './mapStyles';
 
-function useMap(element, center) {
+function useMap(element, center, onLocationChange = () => {}) {
     const [isLoading, setLoading] = useState(true);
     const map = useRef();
+    const overlay = useRef();
+
+    const LatLng = (lat, lng) => {
+        return new window.google.maps.LatLng(lat, lng);
+    }
 
     useEffect(() => {
         if(element.current && center){
-            map.current = new window.google.maps.Map(element.current, {
-                zoom: 12,
-                styles: MapStyles,
-                disableDefaultUI: true,
-                disableDoubleClickZoom: false,
-                center
-            });
+            if(!map.current){
+                map.current = new window.google.maps.Map(element.current, {
+                    zoom: 12,
+                    styles: MapStyles,
+                    disableDefaultUI: true,
+                    disableDoubleClickZoom: false,
+                    center
+                });
+            }
 
+            if(!overlay.current){
+                overlay.current = new window.google.maps.OverlayView();
+                overlay.current.setMap(map.current);
+            }
             setLoading(false);
         }
     }, [element, center]);
 
+    useEffect(() => {
+        if(overlay.current) {
+            const { lat, lng } = center;
+            const factory = {
+                pixelCoords: () => coordsToPixel(overlay.current, LatLng(lat, lng))
+            }
+
+            overlay.current.draw = () => onLocationChange(center, factory)
+        }
+    }, [center, onLocationChange])
+
     return [isLoading, map.current];
 }
+
+function coordsToPixel (overlay, coords) {
+    const projection = overlay.getProjection();
+    return projection ? projection.fromLatLngToContainerPixel(coords) : {};
+};
 
 export default useMap;
