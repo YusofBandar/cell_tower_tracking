999b710c7369b5a56f7687833fbbb3134cdbf725
Yusof Bandar <yusof.bandar@squaredup.com>
2020-09-03 22:17:44 +0100

    Removes transition to accuracy to marker

--- cell-tower-tracking/src/components/common/accuracy/Accuracy.js
+++ cell-tower-tracking/src/components/common/accuracy/Accuracy.js
@@ -1,20 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Accuracy.module.scss';
-import {useEffect} from 'react';
 
 function Accuracy ({ x, y, radius }) {
-    const [accuracy, setAccuracy] = useState(0);
-
-    useEffect(() => {
-        setAccuracy(radius);
-    }, [radius])
-
     return (
         <circle 
             className={ styles.accuracy } 
-            style={{ strokeWidth: accuracy }}
+            style={{ strokeWidth: radius }}
             cx={x} 
             cy={y} 
             r={7}
