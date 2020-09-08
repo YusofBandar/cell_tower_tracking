5310b294aca023877bd5f3ffb3075a10f184fc23
Yusof Bandar <yusof.bandar@squaredup.com>
2020-09-08 22:59:21 +0100

    Reads from config file now

--- cell-tower-tracking/src/API.js
+++ cell-tower-tracking/src/API.js
@@ -1,4 +1,4 @@
-import apiKey from './apiKey'
+import config from './config'
 import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:5501'});
@@ -7,7 +7,7 @@ export const Towers = (lat, lng, range = 300) => API.get(`towers?lat=${lat}&lng=
                                                         .then((response) => response.data)
                                                         .catch((error) => console.error(error));
 
-export const CalculatedLocation = (mcc, mnc, cellTowers) => axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`, 
+export const CalculatedLocation = (mcc, mnc, cellTowers) => axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${config.apiKey}`, 
     { homeMobileCountryCode: mcc, homeMobileNetworkCode: mnc, considerIp: "false", cellTowers})
                                                         .then((response) => response.data)
                                                         .catch((error) => console.error(error));
